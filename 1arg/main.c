9e5a105c07c21d6b73d6f4c3168878a0ba5d8378
Marco Nunnari <marnunna@gmail.com>
2017-04-25 14:01:33 +0200

    1arg

---
+++ 1arg/main.c
@@ -0,0 +1,7 @@
+#include "libftprintf.h"
+
+int	main(int argc, char **argv)
+{
+	ft_printf(argv[1], argv[2]);
+	ft_printf("\n");
+}
--- first_test/main.c
+++
@@ -1,6 +0,0 @@
-#include "libftprintf.h"
-
-int		main()
-{
-	ft_putnbr(ft_printf("hello %s, %s\n", "hello", "world", "I am ft_printf"));
-}
