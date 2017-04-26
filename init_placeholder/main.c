3a6f86efaa26776c02049e1a71a107cfa2bb2017
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-26 20:25:39 +0200

    nc

---
+++ init_placeholder/main.c
@@ -0,0 +1,21 @@
+#include "libftprintf.h"
+
+int		main(int argc, char **argv)
+{
+	t_placeholder *res = init_placeholder();
+
+	ft_putnbr(ft_strlen(res->flags));
+	ft_putstr("\n");
+
+	ft_putnbr(ft_strlen(res->width));
+	ft_putstr("\n");
+
+	ft_putnbr(ft_strlen(res->precision));
+	ft_putstr("\n");
+
+	ft_putnbr(ft_strlen(res->length));
+	ft_putstr("\n");
+
+	ft_putnbr(ft_strlen(res->type));
+	ft_putstr("\n");
+}
