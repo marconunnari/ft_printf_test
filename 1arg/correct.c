1bfd85d3455ff4719a8d359a803e53f6bf75c369
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-26 17:52:11 +0200

    template test

--- 1arg/correct.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	printf(argv[1], argv[2]);
-	printf("\n");
-}
--- 1arg/main.c
+++
@@ -1,7 +0,0 @@
-#include "libftprintf.h"
-
-int	main(int argc, char **argv)
-{
-	ft_printf(argv[1], argv[2]);
-	ft_printf("\n");
-}
