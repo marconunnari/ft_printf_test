1f752fa1d7c3ef3bfa73e1d90146451274c5b34a
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-27 18:27:42 +0200

    nc

---
+++ onechar/correct.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int		main(int argc, char **argv)
+{
+	printf("char: %c\n", argv[1][0]);
+}
---
+++ onechar/main.c
@@ -0,0 +1,6 @@
+#include "libftprintf.h"
+
+int		main(int argc, char **argv)
+{
+	ft_printf("char: %c\n", argv[1][0]);
+}
