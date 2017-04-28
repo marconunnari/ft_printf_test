761e14471fa096389c455cf19bcc27c53d1c300e
Marco Nunnari <marnunna@gmail.com>
2017-04-29 00:35:15 +0200

    hex oct pointers hashtag and ft_asprint

---
+++ onepercent/main.c
@@ -0,0 +1,6 @@
+#include "libftprintf.h"
+
+int		main(int argc, char **argv)
+{
+	ft_printf("percent %%\n");
+}
---
+++ oneptrcmp/main.c
@@ -0,0 +1,13 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include "libftprintf.h"
+
+int		main(int argc, char **argv)
+{
+	char *s = "ciao";
+	char *r1;
+	char *r2;
+	ft_asprintf(&r1, "pointer: %p\n", s);
+	asprintf(&r2, "pointer: %p\n", s);
+	ft_printf("%d\n", ft_strequ(r1, r2));
+}
