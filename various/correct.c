12f10a72f0500a8e9a193adeae292d437f4b3dd3
Marco Nunnari <mnunnari@student.42.us.org>
2017-05-03 19:46:19 +0200

    42filechecker start

---
+++ various/correct.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int		main(int argc, char **argv)
+{
+	printf("@moulitest: %#.x %#.0x\n", 0, 0);
+	printf("@moulitest: %.x %.0x\n", 0, 0);
+	printf("@moulitest: %5.x %5.0x\n", 0, 0);
+	printf("%#08x\n", 42);
+//	printf("%");
+	printf(".%%.%%.%%.%%.%%.%%.%%.%%.");
+}
---
+++ various/main.c
@@ -0,0 +1,11 @@
+#include "libftprintf.h"
+
+int		main(int argc, char **argv)
+{
+	ft_printf("@moulitest: %#.x %#.0x\n", 0, 0);
+	ft_printf("@moulitest: %.x %.0x\n", 0, 0);
+	ft_printf("@moulitest: %5.x %5.0x\n", 0, 0);
+	ft_printf("%#08x\n", 42);
+//	ft_printf("%");
+	ft_printf(".%%.%%.%%.%%.%%.%%.%%.%%.");
+}
