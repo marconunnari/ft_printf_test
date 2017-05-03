d411114a421a14f9467f9727e269cf64cbcf15db
Marco Nunnari <marnunna@gmail.com>
2017-05-04 01:40:59 +0200

    nc

--- various/correct.c
+++ various/correct.c
@@ -2,10 +2,9 @@
 
 int		main(int argc, char **argv)
 {
+	printf("%#08x\n", 42);
 	printf("@moulitest: %#.x %#.0x\n", 0, 0);
 	printf("@moulitest: %.x %.0x\n", 0, 0);
 	printf("@moulitest: %5.x %5.0x\n", 0, 0);
-	printf("%#08x\n", 42);
-//	printf("%");
-	printf(".%%.%%.%%.%%.%%.%%.%%.%%.");
+	printf("%c\n", 0);
 }
--- various/main.c
+++ various/main.c
@@ -2,10 +2,9 @@
 
 int		main(int argc, char **argv)
 {
+	ft_printf("%#08x\n", 42);
 	ft_printf("@moulitest: %#.x %#.0x\n", 0, 0);
 	ft_printf("@moulitest: %.x %.0x\n", 0, 0);
 	ft_printf("@moulitest: %5.x %5.0x\n", 0, 0);
-	ft_printf("%#08x\n", 42);
-//	ft_printf("%");
-	ft_printf(".%%.%%.%%.%%.%%.%%.%%.%%.");
+	ft_printf("%c\n", 0);
 }
