86a00fa6ffa9a2e2bda0ae198b001573cab36bd0
Marco Nunnari <marnunna@gmail.com>
2017-05-05 00:52:16 +0200

    llitle fixes with moulinette

--- undefined/main.c
+++ undefined/main.c
@@ -1,7 +1,8 @@
 #include "libftprintf.h"
+#include <limits.h>
 
 int		main(int argc, char **argv)
 {
 	ft_printf("%");
-//	ft_printf("% ");
 }
+
--- uppers/main.c
+++ uppers/main.c
@@ -6,4 +6,5 @@ int		main(int argc, char **argv)
 	ft_printf("U: %U\n", strtoull(argv[1], NULL, 10));
 	ft_printf("O: %O\n", strtoull(argv[1], NULL, 10));
 	ft_printf("%hU\n", 4294967296);
+	ft_printf("%#.O", 0);
 }
