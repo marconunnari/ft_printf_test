eef5edd5f2bf99ab77b8f1988ce571e5f1f8fedb
Marco Nunnari <mnunnari@student.42.us.org>
2017-05-04 17:49:09 +0200

    basic tests passed

--- various/main.c
+++ various/main.c
@@ -3,10 +3,23 @@
 int		main(int argc, char **argv)
 {
 	ft_printf("%#08x\n", 42);
-	ft_printf("%#08o\n", 42);
-	ft_printf("%# 8x\n", 42);
 	ft_printf("@moulitest: %#.x %#.0x\n", 0, 0);
 	ft_printf("@moulitest: %.x %.0x\n", 0, 0);
 	ft_printf("@moulitest: %5.x %5.0x\n", 0, 0);
 	ft_printf("%c\n", 0);
+	ft_printf("%#08o\n", 42);
+	ft_printf("%# 8x\n", 42);
+	ft_printf("%#-08x\n", 42);
+	ft_printf("@moulitest: %#.o %#.0o\n", 0, 0);
+	ft_printf("%0+5d\n", 42);
+	ft_printf("%05d\n", -42);
+	ft_printf("%0+5d\n", -42);
+	ft_printf("% 10.5d\n", 4242);
+	ft_printf("%+10.5d\n", 4242);
+	ft_printf("%-+10.5d\n", 4242);
+	ft_printf("%03.2d\n", 0);
+	ft_printf("%03.2d\n", 1);
+	ft_printf("%03.2d\n", -1);
+	ft_printf("@moulitest: %.10d\n", -42);
+	ft_printf("%hU\n", 4294967296);
 }
