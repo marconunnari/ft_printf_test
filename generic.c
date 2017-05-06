1e356bb88a2cc6677715025a278dbc5974825453
Marco Nunnari <mnunnari@student.42.us.org>
2017-05-06 18:00:55 +0200

    start restart

--- generic.c
+++ generic.c
@@ -1,6 +1,7 @@
 #include "libftprintf.h"
+#include <stdio.h>
 
 int		main(int argc, char **argv)
 {
-	int res = ft_printf(__FORMAT__, __ARGS__); ft_printf(" r%d\n", res);
+	int res = ft_printf(__FORMAT__, __ARGS__); printf(" r%d\n", res);
 }
