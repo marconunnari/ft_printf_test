fb2a72a4b9a7f7a7e928223a112affe6abce16de
Marco Nunnari <marnunna@gmail.com>
2017-05-08 01:41:45 +0200

    nc

--- srcs/wstr.c
+++
@@ -1,8 +0,0 @@
-#include "libftprintf.h"
-
-int		main()
-{
-	int res;
-	res = ft_printf("%S", L"Heelllooo wooorld 㑄㑅㑆𐀀"); ft_printf(" %d\n", res);
-	res = ft_printf("%S", L"Aሴ𐀀"); ft_printf(" %d\n", res);
-}
