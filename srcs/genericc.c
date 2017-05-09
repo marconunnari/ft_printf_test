ec82898836e1b22fcdafdbcf86e2e2f281eb56f2
Marco Nunnari <marnunna@gmail.com>
2017-05-09 14:40:16 +0200

    nc

--- srcs/genericc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <locale.h>
-#include <limits.h>
-
-int		main(int argc, char **argv)
-{
-	setlocale(LC_ALL, "");
-	int res = printf(__FORMAT____ARGS__); printf(" r%d\n", res);
-}
